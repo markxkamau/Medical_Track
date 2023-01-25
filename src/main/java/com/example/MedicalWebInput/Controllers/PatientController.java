91678c04378f4ad12e8558c45dabbca495077e52
markxkamau <markxkamau@gmail.com>
2023-01-25 22:10:59 +0300

    Deleted unnecessary lines in the patient login

--- src/main/java/com/example/MedicalWebInput/Controllers/PatientController.java
+++ src/main/java/com/example/MedicalWebInput/Controllers/PatientController.java
@@ -104,8 +104,6 @@ public class PatientController {
             return "Patient/patient_login";
         }
         Patient patient = patientService.getPatientByEmail(patientLoginDto.getEmail());
-        model.addAttribute("drug_count", patientService.getDrugByPatientId(patient.getId()).size());
-        model.addAttribute("drug_input", new DrugDto());
         model.addAttribute("patient_data", patient);
         model.addAttribute("drug_info", patientService.getDrugByPatientId(patient.getId()));
         model.addAttribute("schedule_info", scheduleService.getScheduleByPatientId(patient.getId()));
