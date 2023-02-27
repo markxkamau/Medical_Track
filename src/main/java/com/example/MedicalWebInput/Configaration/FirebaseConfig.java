2ef70d1cd330de596b01288e20f840774cda37a5
markxkamau <markxkamau@gmail.com>
2023-02-27 22:31:50 +0300

    Altered filepath in FirebaseConfig to a local document in resources

--- src/main/java/com/example/MedicalWebInput/Configaration/FirebaseConfig.java
+++ src/main/java/com/example/MedicalWebInput/Configaration/FirebaseConfig.java
@@ -3,21 +3,24 @@ package com.example.MedicalWebInput.Configaration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("/home/mark/Coding/Java/FirebaseKey/notification-f00d2-firebase-adminsdk-uolco-b5f98950e1.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream()))
                 .build();
 
         return FirebaseApp.initializeApp(options);
